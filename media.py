74aa643d1858d23bd5a3c2c2ee8d50671e493467
KevinMaFeng <34055984+KevinMaFeng@users.noreply.github.com>
2018-01-06 23:47:14 +0800

    Update media.py
    add docstring style comments.

--- media.py
+++ media.py
@@ -16,25 +16,33 @@ class Movie():
         self.trailer_url = trailer_url
 
     def get_title(self):
+        """get movie title."""
         return self.title
 
     def set_title(self, title):
+        """save new title."""
         self.title = title
 
     def get_storyline(self):
+        """get movie storyline."""
         return self.storyline
 
     def set_storyline(self, storyline):
+        """save new storyline."""
         self.storyline = storyline
 
     def get_poster_image_url(self):
+        """get movie poster image url."""
         return self.poster_image_url
 
     def set_poster_image_url(self, poster_image_url):
+        """save new poster image url."""
         self.poster_image_url = poster_image_url
 
     def get_trailer_url(self):
+        """get movie trailer url."""
         return self.trailer_url
 
     def set_trailer_url(self, trailer_url):
+        """save new trailer url."""
         self.trailer_url = trailer_url
